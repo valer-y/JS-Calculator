50f5b157e3101ce23ad6cc126c9ba5ad25796171
Valery Yazykov <valery.yazykov@innowise-group.com>
2023-03-12 13:55:39 +0100

    Create basic class template with addition and undo operation. Make list of subtasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,47 @@
 "use strict";
 
+/* 1. Make exceptions */
+/* 1.1. Double dot in one number */
+/* 1.2. Double operation symbol input */
+/* 1.3. Zero division */
+
+/* 2. Math operators creating */
+
+/* 3. Float numbers-operations round */
+
 class Calculator {
     constructor() {
         this.value = 0;
+        this.history = [];
+    }
+
+    executeCommand(command) {
+        this.value = command.execute(this.value);
+        this.history.push(this.value);
+    }
+
+    undoCommand() {
+        this.history = this.history.slice(0, -1);
+        return this.history.pop();
     }
 }
 
-const calculator = new Calculator();
-console.log(calculator.value);
+class Add {
+    constructor(addValue) {
+        this.addValue = addValue;
+    }
+
+    execute(currentValue) {
+        return currentValue + this.addValue;
+    }
+}
+
+// Addition
+const calc = new Calculator();
+let test = calc.executeCommand(new Add(15));
+console.log(calc.value, ' ', calc.history);
+
+// Undo Addition
+let testUndo = calc.executeCommand(new Add(25));
+console.log(calc.value, ' ', calc.history);
+console.log('History', calc.undoCommand());
